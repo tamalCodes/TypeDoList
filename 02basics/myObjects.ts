dcf3d8689da3ada4764b3f8f319a3ac8e274b295
Tamal Das <gyansujan69@gmail.com>
2022-12-14 03:50:42 +0530

    fix : studied about objects, functions

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -1,20 +1,16 @@
-// const User = {
-//     name: "hitesh",
-//     email: "hitesh@lco.dev",
-//     isAvtive: true
-// }
-
-//^ Here we are defining the type of the object that the function is accepting
+//& Here we are defining the type of the object that the function is accepting
 function createUser({ name: string, isPaid: boolean }) {}
 createUser({ name: "hitesh", isPaid: false });
 
-//^ Return type of function is an object here with name and price
+//& Return type of function is an object here with name and price
 function createCourse(): { name: string; price: number } {
   return { name: "reactjs", price: 399 };
 }
 
-//^ We can create type for the object and use it in the functions as well
-//^ In the below function we are able to use the User type to define what the function will be accepting and returning
+createCourse();
+
+//& We can create type for the object and use it in the functions as well
+//& In the below function we are able to use the User type to define what the function will be accepting and returning
 
 type User = {
   name: string;
@@ -27,29 +23,15 @@ function createUser2(user: User): User {
   return { name: "", email: "", isActive: true };
 }
 
-// createUser({name: "", email: "", isActive: true})
+//& Read-only properties
+//& We can make the properties of the object read-only by using the readonly keyword
+//& That means we can't change the value of the property after the object is created
 
-// let myUser: User = {
-//   _id: "1245",
-//   name: "h",
-//   email: "h@h.com",
-//   isActive: false,
-// };
-
-// type cardNumber = {
-//   cardnumber: string;
-// };
-
-// type cardDate = {
-//   cardDate: string;
-// };
-
-// type cardDetails = cardNumber &
-//   cardDate & {
-//     cvv: number;
-//   };
-
-// myUser.email = "h@gmail.com";
-// myUser._id = "asa"
+type Product = {
+  readonly _id: string;
+  name: string;
+  price: number;
+  isAvailable: boolean;
+};
 
 export {};
